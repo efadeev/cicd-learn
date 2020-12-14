3201d55dfba82917e3b8a97411ce5ca2585dbca6
Evgeniy Fadeyev <fes94@ya.ru>
2020-12-14 16:14:32 +0300

    Added Terraform

---
+++ docker-instance-aws/docker-instance-aws.tf
@@ -0,0 +1,35 @@
+provider "aws" { }
+
+resource "aws_instance" "my_docker" {
+  ami                    = "ami-0502e817a62226e03"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_docker.id]
+  user_data              = file("docker-install.sh")
+  #key_name               = "keypair"
+}
+
+resource "aws_security_group" "my_docker" {
+  name        = "Docker Server Security Group"
+  description = "Docker Server Security Group"
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
